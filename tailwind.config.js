e96b5c2ca2c4f74469db877a22f78f2eade14ac2
Jared Reisinger <jaredreisinger@hotmail.com>
2022-09-18 17:37:58 -0700

    custom font, show improved logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
+    fontFamily: {
+      sans: ['Gantari', 'sans-serif'],
+      serif: ["'Playfair Display'", 'serif'],
+    },
     container: {
       center: true,
     },
-    extend: {
-      colors: {},
-    },
+    // extend: {
+    //   colors: {},
+    // },
   },
-  variants: {},
+  // variants: {},
   plugins: [require("@tailwindcss/typography")],
 };
